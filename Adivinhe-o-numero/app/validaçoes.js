93935794deebe155cada7e7dc23b5bbc3db01570
Gianluca Laydner <108948959+gianlucaflaydner@users.noreply.github.com>
2022-11-15 23:11:52 -0300

    Add files via upload

---
+++ Adivinhe-o-numero/app/validaçoes.js
@@ -0,0 +1,47 @@
+function verificaChute(chute){
+    const numero = parseInt(chute);
+
+    if(chuteInvalido(numero)){
+        elementoChute.innerHTML += '<div> Chute inválido </div>'
+        return
+    }
+    if(numeroMenorOuMaior(numero)){
+        elementoChute.innerHTML += '<div> Chute inválido, verifique as regras do jogo </div>'
+        return
+    }
+    if(acertouNúmero(numero)){
+        document.body.innerHTML = `<h1>Você acertou</h1>
+        <h2>O número era ${numeroSecreto}!</h2>
+        <button id="jogar-novamente" class="botao_jogar" onclick="reloadPagina()">Jogar novamente</button>
+        `
+    }
+    if(numeroÉMaior(numero)){
+        elementoChute.innerHTML += '<div> O número secreto é maior <i class="fa-solid fa-up-long"></i></div>'
+    }
+    if(numeroÉMenor(numero)){
+        elementoChute.innerHTML += '<div> O número secreto é menor <i class="fa-solid fa-down-long"></i></div>'
+    }
+}
+
+function chuteInvalido(numero){
+    return Number.isNaN(numero);
+}
+
+function numeroMenorOuMaior(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+function acertouNúmero(numero){
+    return numero === numeroSecreto;
+}
+
+function numeroÉMaior(numero){
+    return numero < numeroSecreto;
+}
+function numeroÉMenor(numero){
+    return numero > numeroSecreto;
+}
+
+function reloadPagina(){
+    window.location.reload()
+}
